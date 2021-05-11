71b4d4fbb69982b1d0430bdef2f7a3ffcc3003d4
CorruptComputer <CorruptComputer@users.noreply.github.com>
2021-05-11 00:32:33 +0000

    jekyll build from Action 9fcb9c708f7dece8929682a530df189e8f840959

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+'use strict';
+document.addEventListener('DOMContentLoaded', function () {
+   let homeButton = document.querySelector('#homeButton');
+   let blogButton = document.querySelector('#blogButton');
+   let currentURL = window.location.toString();
+   
+   // I can feel my sanity slipping into the regex abyss
+   let urlRegex = /^http[s]{0,1}:\/\/.+\/[\d]{4}\/[\d]{2}\/[\d]{2}\/.+$/;
+   
+   if (currentURL.includes('/blog') || currentURL.match(urlRegex) !== null) {
+      blogButton.classList.add('selected');
+   } else {
+      homeButton.classList.add('selected');
+   }
+   
+   let spinning = false;
+   let monkey = document.querySelector('#monkey');
+   
+   monkey.addEventListener('click', function () {
+      if (!spinning) {
+         spinning = true;
+         monkey.classList.add('monkeySpin');
+         
+         setTimeout(function () {
+            monkey.classList.remove('monkeySpin');
+            spinning = false;
+         }, 1000);
+      }
+   });
+   
+}, false);
